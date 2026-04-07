5f2dda85bc9403329f51c7f0ca3476384fd88e2f
Alekseiss88 <leha.sergeev.2005@mail.ru>
2026-04-07 23:19:43 +0300

    Add PB12 pin

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define CS_PIN_Pin GPIO_PIN_12
+#define CS_PIN_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
